99c599afad30e78cec123d545aae1c0e1bdab8e5
Raparthi Abhijith <132045783+Abhijith545@users.noreply.github.com>
2026-07-02 19:21:22 +0530

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.java
@@ -0,0 +1,35 @@
+class Solution {
+    private void bfs(int row, int col, char[][] grid, int[][] visit) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        int n = grid.length, m = grid[0].length;
+        queue.offer(new int[] {row, col});
+        while(!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int r = curr[0], c = curr[1];
+            int dx[] = {-1, 0, 1, 0}, dy[] = {0, 1, 0, -1};
+            for (int i = 0; i < 4; i++) {
+                int x = dx[i] + r, y = dy[i] + c;
+                if (x >= 0 && x < n && y >= 0 && y < m && 
+                    grid[x][y] == '1' && visit[x][y] == 0) {
+                    visit[x][y] = 1;
+                    queue.add(new int[] {x, y});
+                }
+            }
+        }
+    }
+
+    public int numIslands(char[][] grid) {
+        int n = grid.length, m = grid[0].length;
+        int[][] visit = new int[n][m];
+        int islands = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (visit[i][j] == 0 && grid[i][j] == '1') {
+                    islands++;
+                    bfs(i, j, grid, visit);
+                }
+            }
+        }
+        return islands;
+    }
+}
